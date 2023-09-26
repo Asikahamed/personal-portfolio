b13c2d768d0f6fd31d4203755be017c69bf0f997
Asikahamed <asikahamed@git.com>
2023-09-26 19:49:48 +0530

    again setting changes

--- personal/settings.py
+++ personal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2fj@e%04k+get+8k$!p7z##-v1*hb1($ngg@crw$5i@$)wh=f&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
